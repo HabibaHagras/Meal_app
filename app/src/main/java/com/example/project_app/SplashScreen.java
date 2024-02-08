3a6f4ade2a1d6899982f56e421ce42a64ef91cd2
HabibaHagras <habibahagras22@gmail.com>
2024-02-08 10:04:40 +0200

    second commit

---
+++ app/src/main/java/com/example/project_app/SplashScreen.java
@@ -0,0 +1,39 @@
+package com.example.project_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+import com.example.project_app.randomMeal.view.RandomMealActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // on below line we are
+                // creating a new intent
+                Intent i = new Intent(SplashScreen.this, RandomMealActivity.class);
+
+                // on below line we are
+                // starting a new activity.
+                startActivity(i);
+
+                // on the below line we are finishing
+                // our current activity.
+                finish();
+            }
+        }, 2000);
+
+
+    }
+}
